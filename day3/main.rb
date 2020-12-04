5b8b01a1673f303b776c9bfc82c131a8382827a4
Chris Kuck <fkuck@covermymeds.com>
2020-12-04 10:58:07 -0500

    day3 part2

--- day3/main.rb
+++ day3/main.rb
@@ -6,4 +6,6 @@ map = File.read(ARGV[0])
 
 world = TreeWorld.new(map.split)
 
-puts world.intersect([0,0],[3,1])
+[[1,1],[3,1], [5,1], [7,1], [1,2]].each do |slope|
+  puts "#{slope.to_s} - #{world.intersect([0,0],slope)}"
+end
