ed2df4274487290603aa9a5427101dc1ca100a4c
Chris Kuck <fkuck@covermymeds.com>
2020-12-05 11:12:06 -0500

    day5 part2
    used uniq -u to help us solve this

--- day5/main.rb
+++ day5/main.rb
@@ -3,9 +3,10 @@ require 'pry'
 exit(1) unless !ARGV[0].nil? && File.exist?(ARGV[0])
 boarding_passes = File.read(ARGV[0]).split
 
+(0..901).each { |i| puts i }
 boarding_passes.each do |pass|
   match = pass.match(/([FB]{7})([LR]{3})/)
   row = match[1].chars.map{ |c| c == "B" ? 1 : 0 }.join.to_i(2)
   col = match[2].chars.map{ |c| c == "R" ? 1 : 0 }.join.to_i(2)
-  puts "#{row*8+col}, row: #{row}, col: #{col}"
+  puts "#{row*8+col}"
 end
